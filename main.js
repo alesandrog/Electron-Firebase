ea4f895f92f59f9b0dd4240cd6f16a76cf6a7040
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-07-22 16:45:39 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Crea la ventana del navegador.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  // Abre las herramientas de desarrollo (DevTools).
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
